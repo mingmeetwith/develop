ea0a6993e655a7280d497d7f27c536d6480c9450
liwenming <wenming.li@centurygame.com>
2023-01-02 12:34:08 +0800

    Init

---
+++ src/main/java/com/company/develop/DevelopApplication.java
@@ -0,0 +1,14 @@
+package com.company.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopApplication.class, args);
+        System.out.println("Application started!");
+    }
+
+}
